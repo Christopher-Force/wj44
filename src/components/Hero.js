8aec85c30170bda866b0255c3819210ed9fbfadf
Christopher Force <chrisforceproduction@outlook.com>
2022-04-03 16:25:44 -0400

    added video poster img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import Will1 from '../assets/video/will.mp4';
+import Will3 from '../assets/img/poster.png'
 
 export default function Hero () {
     return (
         <div className="relative bg-black grid justify-items-center ">
             <div className="md:mx-20 max-w-4xl grid justify-items-center border-double border-4 mb-8 py-4 pt-8">
-                <video className="" style={{objectFit: "fill", width: "70%"}} controls>
+                <video className="" style={{objectFit: "fill", width: "70%"}} controls poster={Will3}>
                     <source id="myvideo" src={Will1} type="video/mp4" className=''/>
                 </video>
             </div>
